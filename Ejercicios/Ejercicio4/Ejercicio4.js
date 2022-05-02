24ad9cc32c7043b7dd3deaef687b3d32e7a0cfef
Alvaro Gómez Tejada <alvarogt17@outlook.es>
2022-05-02 19:20:10 +0200

    Ejercicios de 1smr

---
+++ Ejercicios/Ejercicio4/Ejercicio4.js
@@ -0,0 +1,27 @@
+// En este ejercicio nos piden que solicitemos al usuario un número cualquiera y
+// digamos si este número es par o impar.
+
+// El pseudocódigo es:
+
+//  Proceso Ejercicio04
+//  	Definir numero1 Como Real;
+
+//  	Escribir "Dame un número al Azar";
+
+//  	Leer numero1;
+
+//  	Si numero1 mod 2 = 0 Entonces
+//  		Escribir "Tu Número es par";
+//  	SiNo
+//  		Escribir "Tu Número es impar";
+//  	FinSi
+
+//  FinProceso
+
+let num = parseInt(prompt("Dame un número al azar"));
+
+if (num % 2 == 0) {
+  console.log("El número " + num + " es par.");
+} else {
+  console.log("El número " + num + " es impar.");
+}
---
+++ Ejercicios/Ejercicio5/Ejercicio5.js
@@ -0,0 +1,31 @@
+// En este ejercicio nos piden que digamos si un número dado por el usuario es
+// par o impar y en el caso de que sea 0 marcarlo como que no es ni par, ni impar.
+
+// En pseudocódigo es de la siguiente manera:
+
+//  Proceso Ejercicio04
+
+//  	Definir numero1 Como Real;
+
+//  	Escribir 'Dame un número al Azar';
+//  	Leer numero1;
+
+//  	Si numero1=0 Entonces
+//  		Escribir 'Tu numero es cero por lo cual es neutro';
+//  	SiNo
+//  		Si numero1 MOD 2=0 Entonces
+//  			Escribir 'Tu Número es par';
+//  		SiNo
+//  			Escribir 'Tu Número es impar';
+//  		FinSi
+//  	FinSi
+
+//  FinProceso
+
+let num = parseInt(prompt("Dame un número al azar"));
+
+if (num == 0) {
+  console.log("El número que has elegido es 0 y este es neutro");
+} else if (num % 2 == 0) {
+  console.log("El número " + num + " es par");
+} else console.log("El número " + num + " es impar");
